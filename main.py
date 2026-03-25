e25a434b681dd468ce7b363c9d507c59b087281c
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-25 11:26:22 -0300

    Botei no rabo do enzo, ops botei o input

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ print("Alyrio boca loka")
 print("Olá pessoal")
 print("Guilherme Spec é fiote")
 
+input(f"Digite seu nome:{nome}")
+print(f"{nome}")
 print("Enzo fiotasso da tropa toda kkkkkkk")
 print("tu conhece emporio de alyrio?")
