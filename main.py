24e237d9131c72d755289ac33411788a58df2b52
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-25 09:18:00 -0300

    Fiz um questionamento para ustedes!

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ print("Alyrio boca loka")
 print("Olá pessoal")
 print("Guilherme Spec é fiote")
 
-str(input("Qual seu nome?: "))
-int(input("Qual sua idade?: "))
-
 print("Enzo fiotasso da tropa toda kkkkkkk")
+print("tu conhece emporio de alyrio?")
