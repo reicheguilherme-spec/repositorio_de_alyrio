81093cc8db5b5e52f76faf4f3f0f99351182b5a4
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-25 09:03:40 -0300

    Tentei botar input mas n consegui, ai fui e botei um print, só tmj fiote

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Alyrio boca loka")
 print("Olá pessoal")
 print("Guilherme Spec é fiote")
+
+str(input("Qual seu nome?: "))
+int(input("Qual sua idade?: "))
+
+print("Enzo fiotasso da tropa toda kkkkkkk")
