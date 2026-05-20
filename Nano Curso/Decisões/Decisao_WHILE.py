ae687355d41f8893bfb2b473ec73fd70472812f7
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-05-20 11:33:58 -0300

    alteração no while

---
+++ Nano Curso/Decisões/Decisao_WHILE.py
@@ -0,0 +1,7 @@
+numero=int(input("digite um numero inteiro "))
+while numero<67:
+    print("UUUUUU quase six sevennn")
+    print(numero)
+    numero=numero+1
+if numero == 67:
+    print("SIX SEVEENN")
