e2f95b1654a6e7e4de52642ee17fa2cd473b6f44
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-05-20 11:29:33 -0300

    WHILE

--- Nano Curso/Decisões/Decisao_IF.py
+++ Nano Curso/Decisões/Decisao_IF.py
@@ -1,9 +1,25 @@
 nome=input("Digite o nome: ")
 idade=int(input("Digite a idade: "))
-doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
-if idade>=65:
-    print("O paciente " + nome + " POSSUI atendimento prioritário!")
-elif doenca_infectocontagiosa=="SIM":
-    print("O paciente " + nome + " deve ser direcionado para sala de espera reservada.")
+doenca_infectocontagiosa=input("Suspeita de doença infectocontagiosa? ").upper()
+
+# PRIMEIRO PROBLEMA A SER RESOLVIDO
+if doenca_infectocontagiosa=="SIM":
+    print("Encaminhe o paciente para sala AMARELA")
+elif doenca_infectocontagiosa=="NAO":
+    print("Encaminhe o paciente para sala BRANCA")
 else:
-    print("O paciente " + nome + " NÃO possui atendimento prioritário e pode aguardar na sala comum!")
+    print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
+
+# SEGUNDO PROBLEMA A SER RESOLVIDO
+if idade >= 65:
+    print("Paciente COM prioridade")
+else:
+    genero=input("Digite o gênero do paciente: ").upper()
+    if genero=="FEMININO" and idade>10:
+        gravidez=input("A paciente está grávida? ").upper()
+        if gravidez=="SIM":
+            print("Paciente COM prioridade")
+        else:
+            print("Paciente SEM prioridade")
+    else:
+        print("Paciente SEM prioridade")
